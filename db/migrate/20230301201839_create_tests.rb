63c6a7feec0d044b27824c86fbc2789e801be0e2
Kuper <killkih@mail.ru>
2023-03-03 11:25:44 +0300

    fix2 Task3

--- db/migrate/20230301201839_create_tests.rb
+++ db/migrate/20230301201839_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.string :title, null: false
       t.integer :level, null: false, default: 0
       t.belongs_to :creater , foreign_key: { to_table :users}
-      t.belongs_to :category, foreign_key: true
+      t.belongs_to :categories, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230301201943_create_answers.rb
+++ db/migrate/20230301201943_create_answers.rb
@@ -2,7 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.string :body, null: false
-      t.boolean :correct, default: false
+      t.boolean :correct, null: false, default: false
+      t.belongs_to :questions, null: false
 
       t.timestamps
     end
