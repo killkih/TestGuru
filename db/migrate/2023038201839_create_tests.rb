0851160eafa00c08445bd2ee863f35719e08766e
Kuper <killkih@mail.ru>
2023-03-08 13:14:13 +0300

    fix6 Task3

--- db/migrate/2023038201839_create_tests.rb
+++ db/migrate/2023038201839_create_tests.rb
@@ -3,8 +3,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, null: false, default: 0
-      t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :category, null: false, foreign_key: true
+      t.belongs_to :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
