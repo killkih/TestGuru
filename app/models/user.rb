72626d58ccf552416c574a5d0a3f08804016fb09
Kuper <killkih@mail.ru>
2023-03-10 09:34:51 +0300

    Task 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def show_tests(level)
+    Test.joins('JOIN results ON results.test_id = tests.id')
+    .where("level = ? AND results.user_id = ?", level, id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(name: 'Bob', role: 'creater')
+User.create!(name: 'Tom', role: 'user')
+
+Category.create!(title: 'category1')
+
+Test.create!([{title: 'test1', level: 0, category_id: Category.first.id, author_id: User.order('id').first.id},
+              {title: 'test2', level: 1, category_id: Category.first.id, author_id: User.order('id').first.id},
+              {title: 'test3', level: 2, category_id: Category.first.id, author_id: User.order('id').first.id}])
+
+Test.all.each do |test|
+  Question.create!([{body: 'question1', test_id: test.id},
+                    {body: 'question2', test_id: test.id},
+                    {body: 'question3', test_id: test.id}])
+end
+
+Question.all.each do |question|
+  Answer.create!([{body: 'answer1', correct: false, question_id: question.id},
+                  {body: 'answer2', correct: true, question_id: question.id},
+                  {body: 'answer3', correct: false, question_id: question.id},
+                  {body: 'answer4', correct: false, question_id: question.id}])
+end
+
+Result.create!([{status: true, test_id: 1, user_id: User.order('id').last.id},
+                {status: true, test_id: 2, user_id: User.order('id').last.id},
+                {status: false, test_id: 3, user_id: User.order('id').last.id}])
