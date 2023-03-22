bee18df9419104725b63799752131464a87913a4
Kuper <killkih@mail.ru>
2023-03-22 16:21:53 +0300

    fix2 Task7

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,10 +16,10 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = @test.questions.create(question_params)
+    @question = @test.questions.build(question_params)
 
-    if @question.persisted?
-      show
+    if @question.save
+      redirect_to @question
     else
       render :new
     end
