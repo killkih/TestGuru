2ce415d82c514ffa768dde28d65a415c4c3e30fe
Kuper <killkih@mail.ru>
2023-04-25 19:36:14 +0300

    fix webpack Task-15

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,3 @@
+//= link_tree ../../javascript/images
+//= link_directory ../stylesheets .scss
+//= link application.js
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,16 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import 'bootstrap'
+import 'popper'
+import 'jquery'
+import 'jquery_ujs'
+import 'bootstrap-sprockets'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+Jquery.start()
+Popper.start()
+Bootstrap.start()
